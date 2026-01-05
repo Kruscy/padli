90c8eeab091d01789d405fd1ab012a1d8e5ae5d8
Kruscy <czebeczauer@gmail.com>
2026-01-05 18:40:33 +0100

    Update fallback route to match any path

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.use("/api", routes);
 
 // ---------- FALLBACK ----------
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
